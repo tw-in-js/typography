792d7f1ab49075d75bba474ed8a36066fc4afdd3
Sascha Tandel <s.tandel@kenoxa.de>
2020-12-09 22:06:02 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import type { CSSRules, Plugin, ThemeSection } from '@tw-in-js/core'
+
+export interface Options {
+  className?: string
+}
+
+declare module '@tw-in-js/core' {
+  interface Theme {
+    typography?: ThemeSection<{ css?: CSSRules }>
+  }
+}
+
+import { styles } from './styles'
+
+const typography = ({ className = 'prose' }: Options = {}): Record<string, Plugin> => ({
+  lead: (_, { tag }) => tag('lead'),
+
+  [className]: (parameters, context) => {
+    const color = context.theme('colors', [parameters[0], '600'], '')
+
+    if (color) {
+      return {
+        '& a': { color },
+        '& a code': { color },
+      }
+    }
+
+    const config = styles(context)
+
+    const { css } = config[parameters[0] || 'DEFAULT'] || {}
+
+    if (css) {
+      // TODO merge with styles from theme.typography
+      const rules: CSSRules = {}
+
+      css.forEach((css) =>
+        Object.keys(css).forEach((property) => {
+          const value = css[property]
+
+          const key =
+            value != null && typeof value === 'object' && !Array.isArray(value)
+              ? `& ${property}`
+              : property
+
+          rules[key] = value
+        }),
+      )
+
+      return rules
+    }
+  },
+})
+
+export default typography
